f374d4c939ab2df2f72278a24bfecfcd3cb36404
sgk1988 <georg@science.ssti.ru>
2024-05-13 22:16:52 +0700

    Каркас программы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,54 @@
 #include <iostream>
+#include <unordered_set>
+
+struct Cell
+{
+    size_t x;
+    size_t y;
+};
+
+class Ant
+{
+    Cell start_position;
+    std::unordered_set<Cell> visited_cells;
+
+    int sum_digits(size_t number){
+        int sum = 0;
+        while (number != 0) {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+        return sum;
+
+    }
+
+    int sum_cell_coordinates(Cell& cell){
+        return sum_digits(cell.x) + sum_digits(cell.y);
+    }
+
+    bool is_reachble_cell(Cell& cell){
+        return sum_cell_coordinates(cell) <= 25;
+    }
+
+    public:
+
+    Ant(size_t x, size_t y){
+        start_position = {x,y};
+    }
+
+    void Walk(){
+        visited_cells.clear();
+
+    }
+
+    size_t Available_cells_count(){
+        return visited_cells.size();
+    }
+};
 
 int main(int, char**){
-    std::cout << "Hello, from cppant!\n";
+    Ant ant(1000,1000);
+    ant.Walk();
+    std::cout << "Количество доступных ячеек: " << ant.Available_cells_count() << std::endl;
+    return 0;
 }
